26791f8f6d6b978d92dfa9ad43564b50a2e49a23
Oliver Shih <olivershih@gmail.com>
2021-03-22 17:40:33 -0700

    commented javascript, small fixes in script and css, need to work on more styling and readme

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-var timer = document.querySelector("#timer");
-var startButton = document.querySelector("#start-button");
-var rightWrong = document.querySelector("#right-wrong");
+var timer = document.querySelector("#timer"); //the timer display
+var startButton = document.querySelector("#start-button"); 
+var rightWrong = document.querySelector("#right-wrong"); //the popup message that tells the user if they were correct
+var quizArea = document.querySelector("#quiz-area");
+var hiscoreRanking = document.querySelector("#hiscore-players");
+var hiscorePoints = document.querySelector("#hiscore-scores");
+var resetButton = document.querySelector("#reset");
 
+//questions with options and answer keys
 var q1 = {
     question: "Which is the coolest letter?",
     option: ["A","B","C","π"],
@@ -33,11 +38,19 @@ var q5 = {
 };
 
 var questionPool = [q1,q2,q3,q4,q5];
+//randomizing the order of questions and their respective options
 var randomPool = randomize(questionPool);
 for (var i=0;i<5;i++) {
     randomPool[i].option = randomize(randomPool[i].option);
 }
 
+//set global timer, question index, player names and scores
+var quizTime = 40;
+var questionIndex = 0;
+var playerName = [];
+var hiscoreList = [];
+
+//randomizing items in an array
 function randomize(array) {
     var length = array.length;
     var randomed = new Array(length);
@@ -50,33 +63,35 @@ function randomize(array) {
     return randomed;
 }
 
-var quizTime = 20;
-var questionIndex = 0;
-var quizArea = document.querySelector("#quiz-area");
-var playerName = [];
-var hiscoreList = [];
-var hiscoreRanking = document.querySelector("#hiscore-players");
-var hiscorePoints = document.querySelector("#hiscore-scores");
-var resetButton = document.querySelector("#reset");
-
+//when clicking start button, the button disappears and countdown begins
 startButton.addEventListener("click",function(){
     countDown();
     startButton.style.display = "none";
 });
 
 function countDown() {
+    //display first question
     displayQuestion(randomPool[questionIndex]);
     timer.textContent = quizTime;
+
+    //set coundown by 1-second intervals
     var timerInterval = setInterval(function() {
         
+        //set condition for clearing interval, when questions run out or when timer reaches 0
         if((quizTime <= 0) || (questionIndex>=(randomPool.length))) {
             clearInterval(timerInterval);
+
+            //deletes the quiz area to prevent further attempts at answering
             quizArea.innerHTML = "";
             player = prompt("Game over! your final score is " + quizTime + "! Enter your name:");
+            //add player to hiscores ony if the player entered a non-empty name or didn't choose cancel
             addPlayer((player !== null) && (player !== ""));
+            //local storage
             storePlayer();
+            //display player on hiscores
             displayPlayer();
             
+            //prompt to ask if play again, if yes then refresh page, if no then simply hide timer
             if (confirm("Play Again?")) {
                 location.reload();
             } else {
@@ -89,16 +104,21 @@ function countDown() {
     }, 1000);
 }
 
+//display a specific question in the queue depending on question index
 function displayQuestion(qNum) {
 
+    //display the question
     var questionTitle = document.createElement("h3");
     questionTitle.className = "added";
     questionTitle.textContent = qNum.question;
     quizArea.appendChild(questionTitle);
 
+    //display each of the 4 options
     for (var i=0;i<4;i++) {
         var optionButton = document.createElement("button");
         optionButton.textContent = qNum.option[i];
+
+        //determine if each option is the correct one, and assign respective classes
         if (optionButton.textContent==qNum.correctAnswer) {
             optionButton.className = "added-button correct";
         } else {
@@ -108,17 +128,24 @@ function displayQuestion(qNum) {
     }
 }
 
+//when clicking the quiz area
 quizArea.addEventListener("click",function(event) {
+
     var element=event.target;
+
+    //target the buttons only
     if (element.matches(".added-button")) {
-        rightWrong.setAttribute("style","border-top: 2px solid black");
+        //check if the button has the correct answer, and print the corresponding message
         if (element.matches(".correct")) {
             rightWrong.textContent = "Correct!";
         } else {
             rightWrong.textContent = "Wrong!";
+            //penalty of picking wrong answer, timer -10s
             quizTime -= 10;
         }
     }
+
+    //removes current question, add 1 to question index, and display next question if there's time AND questions left
     quizArea.innerHTML = "";
     questionIndex++;
     if ((questionIndex<randomPool.length) && (quizTime>0)) {
@@ -126,23 +153,30 @@ quizArea.addEventListener("click",function(event) {
     }
 })
 
-function storePlayer() {
-    localStorage.setItem("player names", JSON.stringify(playerName));
-    localStorage.setItem("player scores", JSON.stringify(hiscoreList));
-}
-
+//add the player to hiscores should they choose to
 function addPlayer(boolean) {
-    index = 0;
-    while(quizTime<hiscoreList[index]) {
-        index++;
-    }
     if (boolean){
+        //check for where to insert the player/score depending on rank
+        index = 0;
+        while(quizTime<hiscoreList[index]) {
+            index++;
+        }
+        //separate storage for player name and score, a random choice
         playerName.splice(index,0,player);
         hiscoreList.splice(index,0,quizTime);
     }
 }
 
+//local storage of player name and score
+function storePlayer() {
+    localStorage.setItem("player names", JSON.stringify(playerName));
+    localStorage.setItem("player scores", JSON.stringify(hiscoreList));
+}
+
+//initialize page when refreshed, and display hiscore
 function init() {
+
+    //retrieve local storage to update hiscore list
     var storedHiscore = JSON.parse(localStorage.getItem("player scores"));
     if (storedHiscore !== null) {
       hiscoreList = storedHiscore;
@@ -150,11 +184,13 @@ function init() {
     var storedPlayer = JSON.parse(localStorage.getItem("player names"));
     if (storedPlayer !== null) {
         playerName = storedPlayer;
-        displayPlayer();
     }
+    displayPlayer();
 }
 
+//print player name and score on hiscore, separately
 function displayPlayer() {
+    //for player name
     hiscoreRanking.innerHTML = "";
     for (var i=0;i<playerName.length;i++) {
         var li = document.createElement("li");
@@ -162,6 +198,7 @@ function displayPlayer() {
         hiscoreRanking.appendChild(li);
     }
 
+    //for player score
     hiscorePoints.innerHTML = "";
     for (var i=0;i<hiscoreList.length;i++) {
         var li = document.createElement("li");
@@ -170,7 +207,10 @@ function displayPlayer() {
     }
 }
 
+//removes hiscore when clicking reset
 resetButton.addEventListener("click", function() {
+
+    //clears variable, local storage, and display the new empty list
     playerName = [];
     hiscoreList = [];
     localStorage.clear();
